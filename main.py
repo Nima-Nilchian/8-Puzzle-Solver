5ce8f31195d40f86f014c922a9d9899fbfa83123
Nima-Nilchian <nima.nilchian@gmail.com>
2022-12-24 16:04:24 +0330

    complete IDA* search

--- main.py
+++ main.py
@@ -1,91 +1,175 @@
 import heapq
-
-class Solver:
-    def __init__(self):
-        return
+import math
 
 
 class Puzzle:
-    def __init__(self, n):
+    goal = [1, 2, 3, 4, 5, 6, 7, 8, -1]
+
+
+    def __init__(self, n, board, level):
         self.N = n
-        self.board = [7, 1, 2, -1, 5, 4, 8, 6, 3]      # Default initial board state
+        self.level = level  # g = 0 initially
+        self.board = board  # initial board state
 
-        self.open_list = []
-        self.closed_list = []
-        # making open_list a priority Queue
-        heapq.heapify(self.open_list)
+    def compute_heuristic_manhattan(self):
+        heuristic = 0
+        for num in range(1, 9):
+            distance = abs(self.board.index(num) - self.goal.index(num))
+            i = int(distance / 3)
+            j = int(distance % 3)
+            heuristic = heuristic + i + j
 
+        return heuristic
+
+    def compute_f_value(self):
+        h = self.compute_heuristic_manhattan()
+
+        return h + self.level
 
     def generate_children(self):
-        actions, blank_pos = self.legal_actions()
+        actions = self.legal_actions()
 
         children = []
         for action in actions:
             new_board = self.generate_new_board(action)
-            children.append(new_board)
+            new_puzzle = Puzzle(self.N, new_board, level=self.level + 1)
+
+            children.append(new_puzzle)
 
         return children
 
-    def move_blank(self):
-        actions, blank_pos = self.legal_actions()
-
-        print('You can move in these directions: (', end=' ')
-        print(*actions, end=')')
-        print()
-        action = input('Please choose one: ')
-
-        new_board = self.generate_new_board(action)
-        self.board = new_board
-
-
-    def legal_actions(self):
-        blank_pos = self.blank_position()
-        row, col = blank_pos / self.N, blank_pos % self.N
-
-        actions = ['up', 'down', 'left', 'right']
-        if row == 0:
-            actions.remove('up')
-        if row == self.N:
-            actions.remove('down')
-        if col == 0:
-            actions.remove('left')
-        if col == self.N:
-            actions.remove('right')
-
-        return actions, blank_pos
-
-
     def generate_new_board(self, action):
-        actions, blank_pos = self.legal_actions()
+        blank_pos = self.blank_position()
 
         new_board = self.board.copy()
         if action == 'up':
             new_board[blank_pos], new_board[blank_pos - self.N] = new_board[blank_pos - self.N], new_board[blank_pos]
-        if action == 'down':
+        elif action == 'down':
             new_board[blank_pos], new_board[blank_pos + self.N] = new_board[blank_pos + self.N], new_board[blank_pos]
-        if action == 'left':
+        elif action == 'left':
             new_board[blank_pos], new_board[blank_pos - 1] = new_board[blank_pos - 1], new_board[blank_pos]
-        if action == 'right':
+        elif action == 'right':
             new_board[blank_pos], new_board[blank_pos + 1] = new_board[blank_pos + 1], new_board[blank_pos]
 
         return new_board
 
-    def input_puzzle(self):
-        print("input the start state of puzzle like this pattern:")
-        print("* * * ..."
-              "* * * ..."
-              "* * -1 ...")
+    def legal_actions(self):
+        blank_pos = self.blank_position()
+        row, col = blank_pos // self.N, blank_pos % self.N
 
-        puzzle = []
-        for i in range(self.N):
-            puzz = list(map(int, input().split()))
-            puzzle.extend(puzz)
+        actions = ['up', 'down', 'left', 'right']
+        if row == 0:
+            actions.remove('up')
+        if row == self.N - 1:
+            actions.remove('down')
+        if col == 0:
+            actions.remove('left')
+        if col == self.N - 1:
+            actions.remove('right')
 
-        self.board = puzzle
+        return actions
 
-        return
+    def blank_position(self):
+        pos = 0
+        for i in range(self.N * self.N):
+            if self.board[i] == -1:
+                pos = i
+                return pos
 
-    def solvable(self, puzzle):
+        return pos
+
+    def __str__(self):
+        return str(self.board[0:3]) + '\n' + str(self.board[3:6]) + '\n' + str(self.board[6:9]) + '\n'
+
+    # def move_blank(self):
+    #     actions, blank_pos = self.legal_actions()
+    #     print('You can move in these directions: (', end=' ')
+    #     print(*actions, end=')')
+    #     print()
+    #     action = input('Please choose one: ')
+    #     new_board = self.generate_new_board(action)
+    #     self.board = new_board
+
+
+class Solver:
+    curr_puzzle = None
+
+    def __init__(self, n):
+        self.N = n
+        self.open_list = []
+        self.closed_list = []
+
+        heapq.heapify(self.open_list)  # making open_list a priority Queue
+
+    def process(self):
+        board = self.input_board()  # Get the initial board from user or default
+        puzzle = Puzzle(self.N, board, level=0)  # Create the initial Puzzle
+        self.curr_puzzle = puzzle
+
+        solvable = self.solvable(puzzle)  # Check if the puzzle is Solvable
+        if solvable is False:
+            print('puzzle is not solvable!')
+        else:
+            res = self.ida_star_search(puzzle)
+            print("Path to goal is:")
+            print(*res)
+            return
+
+    def ida_star_search(self, root: Puzzle):
+        bound = root.compute_f_value()
+
+        path = [root]
+        while True:
+            l = self.search(path, bound)
+            if l == 'Found':
+                return path
+            elif l == math.inf:
+                return 'Not Found'
+            bound = l
+
+    def search(self, path, bound):
+        node = path[-1]
+        f = node.compute_f_value()
+        if node.board == node.goal:
+            return 'Found'
+        if f > bound:
+            return f
+
+        min_ = math.inf
+        successors = node.generate_children()
+
+        for suc in successors:
+            if suc not in path:
+                path.append(suc)
+
+                t = self.search(path, bound)
+                if t == 'Found':
+                    return 'Found'
+                if t < min_:
+                    min_ = t
+
+                path.pop(-1)
+
+        return min_
+
+    def input_board(self):
+        print('initial state is:')
+        print('7 1 2\n'
+              '-1 5 4\n'
+              '8 6 3\n')
+        state = input('Continue with initial state or input custom state (y / n)? ')
+        board = []
+        if state == 'y':
+            board = [7, 1, 2, -1, 5, 4, 8, 6, 3]
+        else:
+            print("input the start state of puzzle:")
+            for i in range(self.N):
+                puz = list(map(int, input().split()))
+                board.extend(puz)
+
+        return board
+
+    def solvable(self, puzzle: Puzzle):
         inversions = self.count_inversions(puzzle)
 
         # if N is odd --> Number of inversions should be even
@@ -93,7 +177,7 @@ class Puzzle:
             if inversions % 2 == 0:
                 return True
         else:
-            pos = self.blank_position(puzzle)
+            pos = puzzle.blank_position()
             pos = self.N - pos[0]
 
             # the blank is on an even row counting from the bottom and number of inversions is odd
@@ -106,31 +190,26 @@ class Puzzle:
         return False
 
     def count_inversions(self, puzzle):
-        d1_puzz = []
-        [d1_puzz.extend(puzzle[i]) for i in range(len(puzzle))]
-
+        puz = puzzle.board
         inv = 0
         dims = self.N * self.N
 
         for i in range(dims - 1):
-            if d1_puzz[i] == -1:
+            if puz[i] == -1:
                 continue
-
-            for j in range(i+1, dims):
-                if d1_puzz[j] != -1 and d1_puzz[i] > d1_puzz[j]:
+            for j in range(i + 1, dims):
+                if puz[j] != -1 and puz[i] > puz[j]:
                     inv += 1
         return inv
 
-    def blank_position(self):
-        pos = 0
-        for i in range(self.N * self.N):
-            if self.board[i] == -1:
-                return pos
-
-        return pos
 
 # puzzle = [[1,2,3],[4,5,6],[7,8,-1]]
+# print(puzzle[-1])
+# print(puzzle)
+# b = [7, 1, 2, -1, 5, 4, 8, 6, 3]
 
+s = Solver(3)
+s.process()
 
 # actions = ['up', 'down', 'left', 'right']
 # print(*actions)
